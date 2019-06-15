cccc9ccb09296eb81dfd37ab5a067e06ce670f39
Brian May <brian@linuxpenguins.xyz>
2019-06-15 12:58:52 +1000

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -43,13 +43,13 @@ defmodule RoboticaFace.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:joken, "== 2.0.0-rc2"},
-      {:joken_jwks, git: "https://github.com/joken-elixir/joken_jwks.git"},
+      {:joken, "~> 2.0.0"},
+      {:joken_jwks, "~> 1.0"},
       {:tortoise, "~> 0.9.2"},
       {:guardian, "~> 1.2"},
       {:bcrypt_elixir, "~> 2.0"},
       {:calendar, "~> 0.17.2"},
-      {:timex, "~> 3.1"},
+      {:timex, "~> 3.5"},
       {:distillery, "~> 2.0"}
     ]
   end

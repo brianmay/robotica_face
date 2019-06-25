6177c6543b67b74016c0d4f04c4f7f5a07d43714
Brian May <brian@linuxpenguins.xyz>
2019-06-26 08:35:42 +1000

    Control what processes we start for tests

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule RoboticaFace.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test --no-start"]
     ]
   end
 end
